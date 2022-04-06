23d68907958ec96617c569015ef6211aaa76f115
Brandon - 216049245 <216049245@mycput.ac.za>
2022-04-07 00:42:51 +0200

    Created package called Entity. Created Cinema.java entity using builder pattern.

---
+++ src/main/java/entity/Cinema.java
@@ -0,0 +1,69 @@
+package entity;
+
+public class Cinema {
+
+    private String location;
+    private String nameOfCinema;
+    private String operatingHours;
+
+    // Default Constructor
+    private Cinema (){
+
+    }
+    // Builder Class
+    private Cinema (Builder builder) {
+        this.location = builder.location;
+        this.nameOfCinema = builder.nameOfCinema;
+        this.operatingHours = builder.operatingHours;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getNameOfCinema() {
+        return nameOfCinema;
+    }
+
+    public String getOperatingHours() {
+        return operatingHours;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setNameOfCinema(String nameOfCinema) {
+        this.nameOfCinema = nameOfCinema;
+    }
+
+    public void setOperatingHours(String operatingHours) {
+        this.operatingHours = operatingHours;
+    }
+    public static class Builder {
+        private String location;
+        private String nameOfCinema;
+        private String operatingHours;
+
+
+        public Builder setLocation(String location) {
+            this.location = location;
+            return this;
+        }
+
+        public Builder setNameOfCinema(String nameOfCinema) {
+            this.nameOfCinema = nameOfCinema;
+            return this;
+        }
+
+        public Builder setOperatingHours(String operatingHours) {
+            this.operatingHours = operatingHours;
+            return this;
+        }
+
+        public Cinema build(){
+            return new Cinema(this); //anonymous object (location, name or operating hours.)
+        }
+
+    }
+}
