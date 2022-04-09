6771bcb68168c5a042ff34080a1c8df5421337e7
SiphiweChauque <219084777@mycput.ac.za>
2022-04-09 17:01:05 +0200

    booking entity commit Timeslot entity commit and factory commit for BookingFactory factory commit for TimeslotFactory and Util

---
+++ src/main/java/entity/Timeslot.java
@@ -0,0 +1,82 @@
+package entity;
+
+public class Timeslot<screeningRoom> {
+
+    private screeningRoom screeningRoom;
+    private double Time;
+    private boolean isAvailable;
+
+    public Timeslot(screeningRoom screeningRoom, double Time, boolean isAvailable) {
+        this.screeningRoom = screeningRoom;
+        this.Time = Time;
+        this.isAvailable = isAvailable;
+    }
+
+    private Timeslot(Builder builder) {
+        this.screeningRoom = screeningRoom;
+        this.Time = Time;
+        this.isAvailable = isAvailable;
+    }
+
+    public screeningRoom getScreeningRoom() {
+        return screeningRoom;
+    }
+
+    public double getTime() {
+        return Time;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public class Builder {
+
+        private Object screeningRoom;
+        private double Time;
+        private boolean isAvailable;
+
+        public Builder setScreeningRoom(screeningRoom screeningRoom) {
+            this.screeningRoom = screeningRoom;
+            return this;
+        }
+
+        public Builder setTime(double time) {
+            Time = time;
+            return this;
+        }
+
+        public Builder setAvailable(boolean available) {
+            isAvailable = isAvailable;
+            return this;
+        }
+
+        public Timeslot build() {
+
+            return new Timeslot(this);
+        }
+
+        public Builder copy(Timeslot T) {
+            this.screeningRoom = T.screeningRoom;
+            this.Time = T.Time;
+            this.isAvailable = T.isAvailable;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "Builder{" +
+                    "screeningRoom=" + screeningRoom +
+                    ", Time=" + Time +
+                    ", isAvailable=" + isAvailable +
+                    '}';
+        }
+
+        public Builder setIsAvailable(boolean isAvailable) {
+        return this;
+        }
+    }
+}
+
+
+
